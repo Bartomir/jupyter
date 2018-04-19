cac7cbae6935e82133c70b6241aecdb9d6817f4c
Bartosz <bartosz@Admins-MacBook-Pro-2.local>
2018-04-19 22:38:59 +0200

    more less playable blackjack

--- BlackJack.py
+++ BlackJack.py
@@ -20,11 +20,11 @@ class Card():
 
 
 class Deck():
-    pips = [("A", 11), ("2", 2), ("3", 3), ("4", 4), ("5", 5), ("6", 6), ("7", 7), ("8", 8), ("9", 9), ("10", 10), ("J", 10), ("Q", 10), ("K", 10)] # refine
+    pips = [("A", 11), ("2", 2), ("3", 3), ("4", 4), ("5", 5), ("6", 6), ("7", 7),
+            ("8", 8), ("9", 9), ("10", 10), ("J", 10), ("Q", 10), ("K", 10)]
     suits = ["\u2663", "\u2665", "\u2666", "\u2660"]
 
     def __init__(self):
-        super().__init__()
         self.deck = []
         for suit in self.suits:
             for pip, value in self.pips:
@@ -46,55 +46,122 @@ class Deck():
 
 class Hand():
     def __init__(self):
-        super().__init__()
         self.cards = []
 
     def hit(self, deck):
         self.cards.append(deck.draw_card())
 
     def show(self):
+        print("Your hand: ")
         for card in self.cards:
             card.show()
+        print("Your hand value: {}".format(player.get_value()))
 
     def get_value(self):
-        hand_value = 0
-        for card in self.cards:
-            hand_value += card.value
-        print(hand_value)
+        return validate(sum([card.value for card in self.cards]))
 
+class Player(Hand):
+    def __init__(self, name, money):
+        super().__init__()
+        self.name = name
+        self.money = money
 
+    def __str__(self):
+        return f'{self.name}, {self.money}'
+
+    def bet(self):
+        self.bet = validate(input("How much you want to lose?: "))
+        if validate(self.bet) <= self.money and validate(self.bet) != 0:
+            self.money -= validate(self.bet)
+        else:
+            print("You can't do that!")
 
 class DealerHand(Hand):
-
     def __init__(self):
         super().__init__()
 
     def show(self):
+        print("Dealer's hand: ")
+        for card in self.cards:
+            card.show()
+        print("Dealer's hand value: {}".format(dealer.get_value()))
+
+    def show_hidden(self):
+        print("Dealer's hand: ")
         for card in self.cards[:-1]:
             card.show()
         print(" ___________")
         print("|??         |")
 
+def validate(amount):
+    try:
+        return (decimal.Decimal(amount))
+    except decimal.InvalidOperation:
+        pass
 
-class Player(Hand):
+def show_table():
+    player.show()
+    print("Your hand value: {}".format(player.get_value()))
+    dealer.show_hidden()
 
-    def __init__(self):
-        super().__init__()
-        self.name = input("What's your name punk? ")
-        while True:
-            self.money = self.validator(input("How much do you have? ")) #  ok this is shit, nevermind
-            if self.money > 0:
-                break
+def first_draw():
+    player.hit(deck)
+    player.hit(deck)
+    dealer.hit(deck)
+    dealer.hit(deck)
 
-    def validator(self, amount):
-        try:
-            return (decimal.Decimal(amount).quantize(decimal.Decimal('.01'), rounding=decimal.ROUND_DOWN))
-        except decimal.InvalidOperation:
-            print("{}!? You're nuts or something?".format(amount))
-            return 0
+def game_ongoing():
+    if player.get_value() <= dealer.get_value() and dealer.get_value() <= 21:
+        dealer.show()
+        print("You've lost!!!")
+    else:
+        player.money += player.bet*2
+        dealer.show()
+        print("You lucky bastard!!!")
+        print(player)
 
+def game_end():
+    if player.get_value() > 21:
+        dealer.show()
+        print("You've lost!!!")
 
-    def bet(self, amount):
-        if self.validator(amount) <= self.balance:
-            self.balance -= self.validator(amount)
-            self.check()
+name = input("What's your name punk? ")
+while True:
+    try:
+        money = validate(input("How much do you have? ")) #  ok this is shit, nevermind
+        if money < 10:
+            print("{}!? That's not enough! Show me real money or get the fuck out! ".format(money))
+        elif money % 10 != 0:
+            money = money - (money % 10)
+            print("Spare the change")
+            break
+        else:
+            break
+    except TypeError:
+        print("What!? Are U nuts?")
+
+player = Player(name, money)
+deck = Deck()
+dealer = DealerHand()
+deck.shuffle()
+player.bet()
+first_draw()
+show_table()
+
+while True:
+    choice = input("You hit or stay?: [h/s] ")
+    if choice == 'h':
+        player.hit(deck)
+        player.show()
+        game_end()
+    elif choice == 's':
+        player.show()
+        break
+
+while True:
+    if player.get_value() > dealer.get_value() and dealer.get_value() <= 21:
+        dealer.hit(deck)
+        dealer.show_hidden()
+    else:
+        game_ongoing()
+        break
